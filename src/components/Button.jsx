c4263506cc57e01e597a0b0c9b04c9a987c950e1
nyeinkyawwin02 <nyeinkyawwin11@gmail.com>
2025-10-26 01:33:11 +0630

    Webpack to vite

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
-const Button = () => ({ label = 'Button', onClick }) {
+const Button = ({ label = 'Button', onClick }) => {
   return (
     <button onClick={onClick} style={{ padding: '8px 12px', borderRadius: 6 }}>
       {label}
     </button>
   );
-}
+};
 
-export default Button
+export default Button;
